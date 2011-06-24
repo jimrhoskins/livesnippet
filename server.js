115fbac9c3abfc9da3b29b3845435707eb4ab628
Jim Hoskins <jim@jimhoskins.com>
2011-06-24 10:01:56 -0400

    UI Updates

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 
-var Snippet = require('./lib/snippet').Snippet;
+var Channel = require('./lib/channel').Channel;
 
-// Initialize HTTP Server, Socket, and Snippet database
+// Initialize HTTP Server, Socket, and Channel database
 var app = express.createServer();
 var io = require('socket.io').listen(app);
-var snippets = {};
+var channels = {};
 
 // Utlitity to concat a stream into a string
 function concat(stream, callback) {
@@ -28,24 +28,25 @@ app.configure(function () {
 
 // Endpoint for watch.js to create a new snippet
 app.post('/snippets', function (req, res) {
-  var snippet;
+  var channel;
+
   //TODO add verification;
   if (req.query.channel) {
-    if (snippets[req.query.channel]) {
-      snippet = snippets[req.query.channel];
+    if (channels[req.query.channel]) {
+      channel = channels[req.query.channel];
     } else {
-      snippet = new Snippet(req.query.channel);
-      snippets[snippet.name] = snippet;
+      channel = new Channel(req.query.channel);
+      channels[channel.name] = channel;
     }
   } else {
-    snippet = new Snippet();
-    snippets[snippet.name] = snippet;
+    channel = new Channel();
+    channels[channel.name] = channel;
   }
 
   res.header('Content-Type', 'application/json');
   res.send(JSON.stringify({
-    channel: snippet.name,
-    path: "/snippets/" + snippet.name
+    channel: channel.name,
+    path: "/snippets/" + channel.name
   }));
 });
 
@@ -60,12 +61,12 @@ app.post('/snippets/:channel', function (req, res) {
     res.send('OK');
     
     var channel = req.params.channel;
-    var snippet = snippets[channel];
+    var channel = channels[channel];
     var name = req.query.name;
 
     console.log('Updating...', channel, name);
 
-    snippet.updateFile(name, body);
+    channel.updateFile(name, body);
 
     console.log('Updated Snippet from POST');
   });
@@ -74,7 +75,6 @@ app.post('/snippets/:channel', function (req, res) {
 // Handle sockets
 io.sockets.on('connection', function (socket) {
   var channel = null;
-  var snippet = null;
 
   // Named listener for snippet updates (for easy removal on disconnect)
   function onSnippetUpdate(filename) {
@@ -82,57 +82,30 @@ io.sockets.on('connection', function (socket) {
 
     // Send the socket client the newest content
     var files = filename ? [filename] : null;
-    socket.emit('fileUpdate', snippet.repr(files));
+    socket.emit('fileUpdate', channel.repr(files));
     
   }
 
 
 
   function getChannel(name){
-    if (snippets[name]) {
-      return snippets[name];
+    if (channels[name]) {
+      return channels[name];
     }
-    return (snippets[name] = new Snippet(name));
+    return (channels[name] = new Channel(name));
   }
 
-  socket.on('subscribe', function(channel) {
-    snippet = getChannel(channel);
-    snippet.on('update', onSnippetUpdate);
+  socket.on('subscribe', function(channelName) {
+    channel= getChannel(channelName);
+    channel.on('update', onSnippetUpdate);
     onSnippetUpdate();
-    console.log('SUUUUBBB', channel);
   });
 
   
-  socket.on('imessage', function (data) {
-
-
-
-    // If message contains existing channel, subscribe to that snippet's update (onSnippetUpdate)
-    if (data.channel) {
-      console.log('Attempting to subscribe to channel: ' + data.channel);
-      if (channel === null) {
-        channel = data.channel;
-        if (snippets[channel]) {
-          snippet = snippets[channel];
-        } else {
-          snippet = snippets[channel] = new Snippet(channel);
-        }
-        snippet.on('update', onSnippetUpdate);
-
-        // Initialize the client with the current data
-        onSnippetUpdate();
-        console.log('Successfully subscribed to: ' + channel);
-      } else {
-        console.log('Subscription failed, already subscribed: ' + channel);
-      }
-    }
-  });
-
-
   // On disconnect, remove client's update listener for the snippet
   socket.on('disconnect', function () {
-    if (snippet) {
-      snippet.removeListener('update', onSnippetUpdate);
+    if (channel) {
+      channel.removeListener('update', onSnippetUpdate);
     }
 
     console.log('Client Disconnected');
